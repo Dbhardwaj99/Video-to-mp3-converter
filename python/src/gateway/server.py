df220157e9d6a3302f1b5c523e389cb75559d1fa
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2023-08-23 16:28:44 +0530

    Gateway service code completed

---
+++ python/src/gateway/server.py
@@ -0,0 +1,57 @@
+import os, pika, grdifs, json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config['MONGO_URI'] = "mongodb://host.minikube.internal:27017/videos"
+
+mongo = PyMongo(server)
+
+fs = gridfs.GridFS(mongo.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+    if not err:
+        return token
+    else:
+        return err
+    
+
+@server.route("/upload", methods=["POST"])
+def upload():
+    access, err = validate.token(request)
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        if len(request.files) > 1:
+            return "Only one file allowed", 400
+        elif (len(request.files) < 1):
+            return "No file provided", 400
+        
+        for _, f in request.files.item():
+            err = util.upload(f, fs, channel, access)
+
+            if err:
+                return err
+            
+        return "sucess!", 200
+    else:
+        return "Unauthorized", 401
+    
+@server.route("/download", methods=["GET"])
+def download():
+    pass
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port = 8080)
+
+        
+
